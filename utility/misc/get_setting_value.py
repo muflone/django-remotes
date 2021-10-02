34ad1f5ab65142b25215c96570ecb83e0a25cc45
Muflone <webreg@muflone.com>
2021-10-03 01:55:14 +0200

    Added function get_setting_value

---
+++ utility/misc/get_setting_value.py
@@ -0,0 +1,32 @@
+##
+#     Project: Django Remotes
+# Description: A Django application to execute remote commands
+#      Author: Fabio Castelli (Muflone) <muflone@muflone.com>
+#   Copyright: 2021 Fabio Castelli
+#     License: GPL-3+
+#  This program is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+##
+
+from remotes.models import Setting
+
+
+def get_setting_value(name: str, default_value: str = None) -> str:
+    """
+    Get a value from settings
+    :param name: setting name
+    :param default_value: default value if no setting is found
+    :return:
+    """
+    setting = Setting.objects_enabled.filter(name=name).first()
+    return setting.value if setting else default_value
