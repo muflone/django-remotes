1b7c794b6684ace9ff7b00a734977f69a433f026
Muflone <webreg@muflone.com>
2021-10-03 02:26:14 +0200

    Added app version

--- project/__init__.py
+++ project/__init__.py
@@ -17,3 +17,6 @@
 #  You should have received a copy of the GNU General Public License
 #  along with this program.  If not, see <https://www.gnu.org/licenses/>.
 ##
+
+VERSION = '0.0.1'
+PRODUCT_NAME = 'Django Remotes'
