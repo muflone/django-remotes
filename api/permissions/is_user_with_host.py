78d129b735c3fc8900ea3a404ca823e5cad69367
Muflone <webreg@muflone.com>
2021-10-17 19:26:02 +0200

    New permission IsUserWithHost to authenticate users with host only

---
+++ api/permissions/is_user_with_host.py
@@ -0,0 +1,33 @@
+##
+#     Project: Django Remotes
+# Description: A Django application to execute remote commands
+#      Author: Fabio Castelli (Muflone) <muflone@muflone.com>
+#   Copyright: 2021 Fabio Castelli
+#     License: GPL-3+
+#  This program is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+##
+
+from rest_framework.permissions import IsAuthenticated
+
+from remotes.models import Host
+
+
+class IsUserWithHost(IsAuthenticated):
+    """
+    Authentication for users with host only
+    """
+    def has_permission(self, request, view):
+        host = Host.objects.filter(user_id=request.user.id,
+                                   is_active=True).first()
+        return bool(host)
