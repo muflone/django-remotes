e1ca80d8cea8c8ce674ef85aad64cfff6019511b
Muflone <webreg@muflone.com>
2021-11-01 13:27:01 +0100

    CommandGroup name has to be unique

--- remotes/models/commands_group.py
+++ remotes/models/commands_group.py
@@ -35,6 +35,7 @@ class CommandsGroup(BaseModel):
                                   'CommandsGroup',
                                   'hosts'))
     name = models.CharField(max_length=255,
+                            unique=True,
                             verbose_name=pgettext_lazy(
                                 'CommandsGroup',
                                 'name'))
