12b1e2cddf12b50b9f5b4deeb1c2bbb035911258
Muflone <webreg@muflone.com>
2021-11-14 20:15:06 +0100

    Added method call to WmicParser

--- remotes/client/wmic_parser.py
+++ remotes/client/wmic_parser.py
@@ -22,18 +22,20 @@ import subprocess
 from typing import Optional, Union
 
 WMI_ACTION_GET = 'GET'
+WMI_ACTION_CALL = 'CALL'
 
 
 class WmicParser(object):
     def __init__(self, timeout: int = 10):
         self.timeout = timeout
 
-
     def execute_wmic(self,
                      action: str,
                      alias: str,
                      condition: str = None,
-                     fields: Optional[Union[str, list, tuple]] = None) -> str:
+                     fields: Optional[Union[str, list, tuple]] = None,
+                     method: str = None,
+                     parameters: Optional[list[str]] = None) -> str:
         """
         Execute an action for a method from wmic for the specified role alias
 
@@ -41,6 +43,8 @@ class WmicParser(object):
         :param alias: WMI role alias to use
         :param condition: a condition to filter results
         :param fields: field list to return during GET request
+        :param method: method name to execute
+        :param parameters: list of arguments passed to the method
         :return: resulting string from wmic output
         """
         # Prepare arguments
@@ -62,6 +66,19 @@ class WmicParser(object):
                 # Field list as a string
                 arguments.append(fields)
             arguments.append('/VALUE')
+        elif action == WMI_ACTION_CALL:
+            # Call method with arguments
+            arguments.append(method)
+            if parameters:
+                # Add parameters in the form ['(param1,', 'param2,', ..., ')']
+                for index, parameter in enumerate(parameters):
+                    if index == 0:
+                        parameter = f'({parameter}'
+                    if index == len(parameters) - 1:
+                        parameter = f'{parameter})'
+                    else:
+                        parameter = f'{parameter},'
+                    arguments.append(parameter)
         process = subprocess.run(args=arguments,
                                  capture_output=True,
                                  timeout=self.timeout)
@@ -143,3 +160,45 @@ class WmicParser(object):
                 name, value = line.split('=', 1)
                 item[name] = value
         return results
+
+    def call(self,
+             alias: str,
+             condition: str = None,
+             method: str = None,
+             parameters: Optional[list[str]] = None) -> tuple[int,
+                                                              dict[str],
+                                                              str]:
+        """
+        Call a method from wmic for the specified role alias
+
+        :param alias: WMI role alias to use
+        :param field: single field to return
+        :param condition: a condition to filter results
+        :param method: method name to execute
+        :param parameters: list of arguments passed to the method
+        :return: tuple with return code, parameters and results from wmic call
+        """
+        results = {}
+        wmic_output = self.execute_wmic(action=WMI_ACTION_CALL,
+                                        alias=alias,
+                                        condition=condition,
+                                        method=method,
+                                        parameters=parameters)
+        results_arguments_block = False
+        return_code = None
+        for line in wmic_output.split('\n'):
+            line = line.strip('\r')
+            line = line.strip('\n')
+            # Begin of arguments block
+            if line == 'instance of __PARAMETERS':
+                results_arguments_block
+            elif results_arguments_block and line == '};':
+                # End of arguments block
+                break
+            elif line.startswith('\t'):
+                # Output values
+                name, value = line[1:].split(' = ', 1)
+                results[name] = value
+                if name == 'ReturnValue':
+                    return_code = int(value.rstrip(';'))
+        return return_code, results, wmic_output
