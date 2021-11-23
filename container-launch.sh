f949e4aa02b42e95c0e17902905cfc6845e3e3b5
Muflone <webreg@muflone.com>
2021-11-23 23:56:29 +0100

    Added container related files

---
+++ container-launch.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+##
+#     Project: Django Remotes
+# Description: A Django application to execute remote commands
+#      Author: Fabio Castelli (Muflone) <muflone@muflone.com>
+#   Copyright: 2021 Fabio Castelli
+#     License: GPL-3+
+#  This program is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+##
+
+cd /app
+export DJANGO_SETTINGS_MODULE='project.settings_container'
+python manage.py migrate
+python manage.py migrate --database api_logs
+python manage.py collectstatic --noinput
+python manage.py runserver 0.0.0.0:${SERVER_PORT:-8080}
